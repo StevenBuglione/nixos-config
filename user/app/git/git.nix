b3d7c5291333fbb97fdeb16a32fa0a767f09a4ff
Steven Buglione <>
2024-10-20 22:54:20 -0400

    Update

--- user/app/git/git.nix
+++ user/app/git/git.nix
@@ -2,12 +2,24 @@
 
 {
   home.packages = [ pkgs.git ];
-  programs.git.enable = true;
-  programs.git.userName = userSettings.name;
-  programs.git.userEmail = userSettings.email;
-  programs.git.extraConfig = {
-    init.defaultBranch = "main";
-    safe.directory = [ ("/home/" + userSettings.username + "/.dotfiles")
-                       ("/home/" + userSettings.username + "/.dotfiles/.git") ];
+
+  programs.git = {
+    enable = true;
+    userName = userSettings.name;
+    userEmail = userSettings.email;
+    extraConfig = {
+      gpg = {
+        format = "ssh";  # Set GPG format to SSH
+      };
+      "gpg \"ssh\"" = {
+        program = "${pkgs._1password-gui}/bin/op-ssh-sign";  # Corrected path for 1Password's SSH signing program
+      };
+      commit = {
+        gpgsign = true;  # Enable GPG signing for commits
+      };
+      user = {
+        signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIADXEW0ESKUfvgzAYIuHH/Rehcvhm8j4op7VlpLClfvC";
+      };
+    };
   };
 }
