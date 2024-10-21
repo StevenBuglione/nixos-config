f0c8866bc87a0e0fabd359261d6cd03f850c4a35
Steven Buglione <stevenbuglione1@gmail.com>
2024-10-20 21:34:24 -0400

    Update

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -8,79 +8,32 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "radeon" ];
-  boot.initrd.kernelModules = [ "radeon" ];
-  boot.kernelModules = [ "kvm-amd" "radeon" ];
-  boot.extraModulePackages = with config.boot.kernelPackages; [ zenpower ];
-  hardware.opengl.extraPackages = [ pkgs.amdvlk pkgs.rocmPackages.clr.icd ];
-  hardware.opengl.extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
-
-  systemd.tmpfiles.rules = [
-    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
-  ];
-
-  # my stupid usb hub crashes systemct suspend half of the time now
-  # https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Sleep_hooks
-  systemd.services.root-suspend = {
-    enable = true;
-    description = "Root systemd suspend prehook";
-    unitConfig = {
-      Description = "Root systemd suspend prehook";
-      Before = "sleep.target";
-    };
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.uhubctl}/bin/uhubctl -a off";
-    };
-    wantedBy = [ "sleep.target" ];
-  };
-  systemd.services.root-resume = {
-    enable = true;
-    description = "Root systemd suspend posthook";
-    unitConfig = {
-      Description = "Root systemd suspend posthook";
-      After = "suspend.target";
-    };
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.uhubctl}/bin/uhubctl -a on";
-    };
-    wantedBy = [ "suspend.target" ];
-  };
-
-  boot.kernel.sysctl = {
-    "vm.swappiness" = 90;
-    "vm.vfs_cache_pressure" = 50;
-    "vm.dirty_background_ratio" = 2;
-    "vm.dirty_ratio" = 5;
-  };
-
-  services.btrfs.autoScrub = {
-    enable = true;
-    interval = "weekly";
-  };
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/637d8261-0650-4ece-a35b-59d97baf64a7";
-      fsType = "btrfs";
-      options = [ "noatime,compress-force=zstd:2,discard=async,commit=120,clear_cache,space_cache=v2,subvol=@" ];
+    { device = "/dev/disk/by-uuid/e2b5d740-eb3c-4239-ad53-ff4a079595ab";
+      fsType = "ext4";
     };
-
-  boot.initrd.luks.devices."luks-385106b5-71f7-460e-9a2b-2416f3b54cb6".device = "/dev/disk/by-uuid/385106b5-71f7-460e-9a2b-2416f3b54cb6";
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/F09D-73C9";
+    { device = "/dev/disk/by-uuid/E3CC-BC42";
       fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/4f95c00a-a67e-42ee-a4cc-81206b882a46"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
